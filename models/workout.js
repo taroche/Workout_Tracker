98cca0a0c60578e86ea7f2e94ac2543e09ba2dbd
Tristan LaRoche <laroche11235@gmail.com>
2021-04-17 11:07:36 -0400

    add exercises to schema

--- models/workout.js
+++ models/workout.js
@@ -5,5 +5,35 @@ const WorkoutSchema = new Schema({
     day: {
         type: Date,
         default: Date.now
+      },
+      exercises: [
+        {
+          type: {
+            type: String,
+            trim: true,
+            required: true
+          },
+          name: {
+            type: String,
+            trim: true,
+            required: true
+          },
+          duration: {
+            type: Number,
+            required: true
+          },
+          weight: {
+            type: Number
+          },
+          reps: {
+            type: Number
+          },
+          sets: {
+            type: Number
+          },
+          distance: {
+            type: Number
+          }
       }
+    ]
 })
