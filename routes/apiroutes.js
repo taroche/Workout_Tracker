1df83286cfdb031bc75efb7e940e2b560366afd4
Tristan LaRoche <laroche11235@gmail.com>
2021-04-17 11:31:19 -0400

    App.post to submit new completed workouts

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -8,12 +8,19 @@ module.exports = function(app) {
         .catch(err => {
             res.status(400).json(err);
         });
-    })
+    });
     app.get("/api/workouts/range", ({}, res) => {
         db.Workout.find({}).then((dbWorkout) => {
           res.json(dbWorkout);
         }).catch(err => {
           res.status(400).json(err);
         });
+      });
+      app.post("/api/workouts/", (req, res) => {
+        db.Workout.create(req.body).then((dbWorkout) => {
+          res.json(dbWorkout);
+        }).catch(err => {
+            res.status(400).json(err);
+          });
       });
 }
