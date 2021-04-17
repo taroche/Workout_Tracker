cee5d14bc6bc910ba250a477498d03559bf7a797
Tristan LaRoche <laroche11235@gmail.com>
2021-04-17 11:27:18 -0400

    get exercise html

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,3 +1,7 @@
 const path = require("path");
 
-module.exports = function(app) {}
+module.exports = function(app) {
+    app.get("/exercise", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
+      });
+}
