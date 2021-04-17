74c87b511a12bc6a8b45ff9623701b2b7408c502
Tristan LaRoche <laroche11235@gmail.com>
2021-04-17 11:30:24 -0400

    App.get to pull up info for the workouts page

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,3 +1,12 @@
 const db = require("../models");
 
-module.exports = function(app) {}
+module.exports = function(app) {
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.find({}).then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+    })
+}
